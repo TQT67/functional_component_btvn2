ee42a335905339c74859ab43684d7129871c23a3
TQT67 <trangtrinh000000@gmail.com>
2023-02-17 14:57:20 +0700

    btvn2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      const date = new Date().toLocaleTimeString();
+      setTime(date);
+    }, 5000);
+    return () => clearTimeout(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Giờ hiện tại là: {time}</h1>
     </div>
   );
-}
+};
 
 export default App;
